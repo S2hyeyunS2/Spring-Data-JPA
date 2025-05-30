7e46f4ffd81ed47260c2a12561c64a7e8f688186
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-05-30 19:25:42 +0900

    [FEAT] Pageable 추가

--- src/main/java/com/ll/jpa/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/jpa/domain/post/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.ll.jpa.domain.post.post.repository;
 
 import com.ll.jpa.domain.post.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,4 +15,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByOrderByIdDesc();
     List<Post> findTop2ByTitleLikeOrderByIdDesc(String title);
     List<Post> findTop2ByOrderByIdDesc();
+    Page<Post> findByTitleLike(String title, Pageable pageable);
 }
