d2e4b74775baf116e6ca84bebc258cc2ad3503d4
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-05-30 10:32:43 +0900

    [FEAT] REMOVE 가 없어서 자식들이 먼저 삭제되지 않아서 외래키 제약에 의해 삭제 실패

--- src/main/java/com/ll/jpa/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jpa/global/initData/BaseInitData.java
@@ -1,6 +1,5 @@
 package com.ll.jpa.global.initData;
 
-import com.ll.jpa.domain.post.comment.entity.PostComment;
 import com.ll.jpa.domain.post.comment.service.PostCommentService;
 import com.ll.jpa.domain.post.post.entity.Post;
 import com.ll.jpa.domain.post.post.service.PostService;
@@ -11,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,7 +25,6 @@ public class BaseInitData {
         return args -> {
             self.work1();
             self.work2();
-            self.work3();
         };
     }
 
@@ -39,8 +35,6 @@ public class BaseInitData {
         Post post1 = postService.write("title1", "content1");
         Post post2 = postService.write("title2", "content2");
         Post post3 = postService.write("title3", "content3");
-
-        post1.setTitle("title1-1");
 
         post1.addComment(
                 "comment1"
@@ -57,27 +51,7 @@ public class BaseInitData {
 
     @Transactional
     public void work2() {
-        Post post = postService.findById(1).get();
-        System.out.println("1번글 로드 완료");
-
-        List<PostComment> postComments = post.getComments();
-        System.out.println("1번글의 댓글들 로드 완료");
-
-        PostComment postComment1 = postComments.get(0);
-        System.out.println("1번글의 첫번째 댓글 로드 완료");
-
-        PostComment postComment2 = postComments.get(1);
-        System.out.println("1번글의 두번째 댓글 로드 완료");
-    }
-
-    @Transactional
-    public void work3() {
         Post post1 = postService.findById(1).get();
-
-        post1.getComments().size();
-
-        post1.addComment("comment4");
-
-        post1.getComments().get(2);
+        postService.delete(post1);
     }
 }
